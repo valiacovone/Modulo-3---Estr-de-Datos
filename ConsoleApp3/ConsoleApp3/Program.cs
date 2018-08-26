ea33c17579d5c886e25100dd416365b117016072
Val Iacovone <vale.iacovone@gmail.com>
2018-08-26 17:33:37 -0300

    Programa

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 1 Funciona OK
+
+            //string usr;
+            //string pass;
+            //do
+            //{
+            //    Console.WriteLine("Ingrese el usuario");
+            //    usr = Console.ReadLine();
+            //    Console.WriteLine("Ingrese la contraseña");
+            //    pass = Console.ReadLine();
+            //}
+            //while ((usr != "1234") && (pass != "1111"));
+
+            //Console.ReadKey();
+            //Ejercicio 2 Funciona 
+            //Console.WriteLine("Ingrese un numero entero");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int asd = 1;
+
+            //if (n<=9 && n>=0)
+            //{
+            //    Console.WriteLine("El numero tiene una cifra");
+            //}
+            //else
+            //{
+            //    while (n>=10)
+            //    {
+            //        n = n / 10;
+            //        asd++;
+            //    }
+            //    Console.WriteLine("El numero tiene " + asd + " cifras");
+            //}
+            //Console.ReadKey();
+            //string num = Console.ReadLine();
+            //int con = 0;
+
+            //foreach (int element in num)
+            //{
+            //    con++;
+            //}
+            //Console.WriteLine("Su numero tiene " + con + " cifras");
+            //Console.ReadKey(); 
+
+            //Ejercicio 3
+            // int num = 1;
+            // int cont = 1;
+
+            //while (num < 90)
+            //{
+
+            //    num = cont * 9;
+            //     Console.WriteLine(cont + " por 9 es: " + num );
+            //     cont++;
+            //}
+            //Console.ReadKey();
+
+            //Ejercicio 4
+            //int num = 1;
+
+            //while (num > 0)
+            //{
+            //    Console.WriteLine(num);
+            //    num++;
+            //} 
+
+            //Ejercicio 5
+            //int no = 0;
+            //while (no != 1) {
+            //    Console.Write("Hola");
+            //} 
+
+            //----!!!!!!!!!!!!!!!!!!! FALTA EJERCICIO 6 Y 7
+
+
+            //Ejercicio 8 
+            //Console.WriteLine("Ingrese una letra");
+            //string letra = Console.ReadLine().ToLower();
+
+            //if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u")
+            //        { 
+            //    Console.WriteLine("La letra es una vocal");
+            //}
+            //else {
+            //    Console.WriteLine("La letra es una consonante");
+            //}
+
+            //Console.ReadKey();
+
+            //Ejercicio 9
+
+            //Console.WriteLine("Ingrese un usuario");
+            //string usr = Console.ReadLine();
+            //string pass1;
+            //string pass2;
+            //do
+            //{
+
+            //    Console.WriteLine("Ingrese una contraseña");
+            //    pass1 = Console.ReadLine();
+            //    Console.WriteLine("Ingrese la misma contraseña que ingreso previamente");
+            //    pass2 = Console.ReadLine();
+            //}
+            //while (pass1 != pass2);
+            //if (pass1 == pass2)
+            //{
+            //    Console.WriteLine("Las contraseñas coinciden. Gracias por usar el programa");
+            //}
+
+            //Console.ReadKey(); 
+
+            //Ejercicio 10
+            //Console.WriteLine("Ingrese un nombre");
+            //string usr = Console.ReadLine();
+
+            //if (usr == "Martin")
+            //{
+            //    Console.WriteLine("Hola");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No te conozco");
+            //}
+            //Console.ReadKey();
+        }
+
+    }
+}
